d5836d2dbcff423227da5bf04e9a806be0e5d901
yann <yann@needsome.coffee>
2025-09-05 16:46:17 +0200

    adapt with new path/views

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -1,13 +1,15 @@
 from django.contrib import admin
 from django.urls import path
-
 from . import views
+import lettings.views
+import profiles.views
+
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('lettings/', views.lettings_index, name='lettings_index'),
-    path('lettings/<int:letting_id>/', views.letting, name='letting'),
-    path('profiles/', views.profiles_index, name='profiles_index'),
-    path('profiles/<str:username>/', views.profile, name='profile'),
+    path('lettings/', lettings.views.index, name='lettings_index'),
+    path('lettings/<int:letting_id>/', lettings.views.letting, name='letting'),
+    path('profiles/', profiles.views.index, name='profiles_index'),
+    path('profiles/<str:username>/', profiles.views.profile, name='profile'),
     path('admin/', admin.site.urls),
 ]
