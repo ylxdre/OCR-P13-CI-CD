b40a7d90301cd4047b48f6c05ceaec7006ad8033
yann <yann@needsome.coffee>
2025-09-05 16:56:43 +0200

    display more

--- oc_lettings_site/admin.py
+++ oc_lettings_site/admin.py
@@ -1,10 +1,18 @@
 from django.contrib import admin
-
 from lettings.models import Letting
 from lettings.models import Address
 from profiles.models import Profile
 
 
-admin.site.register(Letting)
-admin.site.register(Address)
-admin.site.register(Profile)
+class LettingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'address')
+
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ('id', 'number', 'street', 'city', 'country_iso_code')
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'favorite_city')
+
+admin.site.register(Letting, LettingAdmin)
+admin.site.register(Address, AddressAdmin)
+admin.site.register(Profile, ProfileAdmin)
