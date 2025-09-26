aa9dfbb7c3bc8b689bf8fc5cee0ae442eb8ee352
yann <yann@needsome.coffee>
2025-09-26 11:06:28 +0200

    add extensions

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,10 @@ release = '1.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+        'sphinx_rtd_theme',
+        'groundwork',
+        ]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
